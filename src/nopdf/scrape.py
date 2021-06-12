919c742dc354bbcf8080fd557999643a1783d426
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-12 10:36:44 +0530

    Implemented scrape

--- src/nopdf/scrape.py
+++ src/nopdf/scrape.py
@@ -1,7 +1,18 @@
-"""Scrape"""
+"""Scrape."""
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+
+from utils import www
+from utils.cache import cache
 
 
-def scrape():
+@cache('nopdf', 86400)
+def scrape(url):
     """Run."""
-    return True
-        
+    html = www.read(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    domain = urlparse(url).netloc
+    return list(map(
+        lambda img: 'https:/%s%s' % (domain, img.get('src')),
+        soup.find_all('img'),
+    ))
--- src/nopdf/tests/test_scrape.py
+++ src/nopdf/tests/test_scrape.py
@@ -4,15 +4,17 @@ import unittest
 
 from nopdf import scrape
 
+TEST_URL = 'https://www.dgi.gov.lk' \
+    + '/news/press-releases-sri-lanka/covid-19-documents'
+
 
 class TestScrape(unittest.TestCase):
     """Tests."""
 
     def test_scrape(self):
         """Test."""
-        self.assertTrue(scrape.scrape())
+        self.assertGreater(len(scrape.scrape(TEST_URL)), 0)
 
 
 if __name__ == '__main__':
     unittest.main()
-    

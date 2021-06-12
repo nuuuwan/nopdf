91e522125a1ed6e6a214519d1ab5c8ccfdd8ce64
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-12 14:09:05 +0530

    Fixed various lint errors

--- src/nopdf/custom_dgigovlk.py
+++ src/nopdf/custom_dgigovlk.py
@@ -1,5 +1,6 @@
 """CustomDgigovlk."""
 import re
+import logging
 
 from utils import www
 
@@ -7,12 +8,14 @@ from nopdf import scrape, ocr
 
 URL = 'https://www.dgi.gov.lk/news/press-releases-sri-lanka/covid-19-documents'
 
-REGEX_MEDIA_URL = r'.+/(?P<date>\d{4}\.\d{2}\.\d{2})/.+(?P<ref_no>\d{3}).+-page-(?P<page_no>\d{3}).+'
+REGEX_MEDIA_URL = r'.+/(?P<date>\d{4}\.\d{2}\.\d{2})/.+' \
+    + r'(?P<ref_no>\d{3}).+-page-(?P<page_no>\d{3}).+'
+
 
 def custom_dgigovlk():
     """Run custom."""
     media_url_list = scrape.scrape(URL)
-    for i, media_url in enumerate(media_url_list):
+    for media_url in media_url_list:
         if not any([
             'Press_Release' in media_url,
             'PR_' in media_url,
@@ -29,13 +32,16 @@ def custom_dgigovlk():
         ref_no = info['ref_no']
         page_no = info['page_no']
 
-        base_name = '/tmp/nopdf.dgigovlk.%s.ref%s.page%s' % (date, ref_no, page_no)
+        base_name = '/tmp/nopdf.dgigovlk.%s.ref%s.page%s' % (
+            date,
+            ref_no,
+            page_no,
+        )
         text_file = '%s.txt' % (base_name)
         image_file = '%s.jpeg' % (base_name)
 
         www.download_binary(media_url, image_file)
         ocr.ocr(image_file, text_file)
-
 
 
 if __name__ == '__main__':

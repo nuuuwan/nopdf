4d7071a6a39671be36cae05b00ef36b4a185f703
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-13 10:04:57 +0530

    Fixed invalid URL issue

--- src/nopdf/scrape.py
+++ src/nopdf/scrape.py
@@ -10,14 +10,14 @@ from utils.cache import cache
 logging.basicConfig(level=logging.DEBUG)
 
 
-@cache('nopdf', 86400)
+@cache('nopdf', 3600)
 def scrape(url):
     """Run."""
     html = www.read(url)
     soup = BeautifulSoup(html, 'html.parser')
     domain = urlparse(url).netloc
     media_url_list = list(map(
-        lambda img: 'https:/%s%s' % (domain, img.get('src')),
+        lambda img: 'https://%s%s' % (domain, img.get('src')),
         soup.find_all('img'),
     ))
 

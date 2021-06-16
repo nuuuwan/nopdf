2ab4ab22d474313675003a8276cc6e1f09eda16f
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-16 08:04:17 +0530

    Added publish_to_twitter

---
+++ src/nopdf/custom_dgigovlk_covid19/publish_to_twitter.py
@@ -0,0 +1,16 @@
+"""Publish to Twitter."""
+import json
+import tweepy
+
+def publish_to_twitter(
+    data,
+    api_key,
+    api_secret_key,
+    access_token,
+    access_token_secret,
+):
+    """Publish to Twitter."""
+    auth = tweepy.OAuthHandler(api_key, api_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    print(api.update_status(json.dumps(data)))
